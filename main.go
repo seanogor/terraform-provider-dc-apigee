21746dfd9b5948bea91d76bc5170dab57a6db0a5
Seanogor <seanogor@gmail.com>
2024-08-10 09:32:51 +0000

    remove unnecessary import

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/moodys-ma-external/kyc-apigee/tf-provider/dc-apigee/provider"
 )
